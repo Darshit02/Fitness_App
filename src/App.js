528ac785b574813fe3ba8b80276f86f2893be65d
LazY <darshitmistry2@gmail.com>
2023-07-30 20:37:26 +0530

    A Navbar styling done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './App.css'
+import { Route ,Routes} from 'react-router-dom';
+import { Box } from '@mui/material'
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import ExerciseDetail from './components/ExerciseDetail';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{ width: {xl:'1488px'}}} m="auto">
+        <Navbar/>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
+        </Routes>
+        <Footer/>
+    </Box>
   )
 }
 
