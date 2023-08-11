27e1079de90b567d7d8d944dced8475ae9cfa1c8
LazY <darshitmistry2@gmail.com>
2023-08-11 10:20:24 +0530

    A Exercise fetching was done and bodyPart components was done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,28 @@
-import React ,{useState}from 'react'
-import { Box } from '@mui/material'
-import Exercises from '../components/Exercises'
-import SearchExercises from '../components/SearchExercises'
-import HeroBenner from '../components/HeroBenner'
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
+import HeroBenner from "../components/HeroBenner";
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercise, setExercises] = useState([]);
+
   return (
     <>
-    <Box>
-      <HeroBenner/>
-      <SearchExercises/>
-      <Exercises/>
-    </Box>
+      <Box>
+        <HeroBenner />
+        <SearchExercises
+          setExercises={setExercises}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
+        <Exercises  setExercises={setExercises}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}/>
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
