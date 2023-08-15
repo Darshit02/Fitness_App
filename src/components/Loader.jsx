311506c80e06e11639995be5c4070b6100e16f6c
LazY <darshitmistry2@gmail.com>
2023-08-15 15:12:56 +0530

    Loading chenged

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { CircularProgress, Stack } from '@mui/material';
-
+import { Stack } from '@mui/material';
+import { InfinitySpin } from 'react-loader-spinner';
 
 const Loader = () => (
   <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
-    <CircularProgress color="inherit" />
+    <InfinitySpin color="grey" />
   </Stack>
 );
 
