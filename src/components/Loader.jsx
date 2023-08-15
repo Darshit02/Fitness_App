bd4a2941003f47b8496b2c2f93813b3110e39488
LazY <darshitmistry2@gmail.com>
2023-08-15 15:02:54 +0530

    loading animation was inited

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Stack } from '@mui/material';
-import { InfinitySpin } from 'react-loader-spinner';
+import { CircularProgress, Stack } from '@mui/material';
+
 
 const Loader = () => (
   <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
-    <InfinitySpin color="grey" />
+    <CircularProgress color="inherit" />
   </Stack>
 );
 
